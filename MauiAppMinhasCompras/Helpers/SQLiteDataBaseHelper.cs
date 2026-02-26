ff3c630e8aae28b17fa69da313f46b2df90158da
Victor <Victor@DESKTOP-KHPNUNT>
2026-02-26 16:09:03 -0300

    connection e primeiro create table

--- MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDataBaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDataBaseHelper(string Path)
+        {
+            _conn = new SQLiteAsyncConnection(Path);
+            _conn.CreateTableAsync<Models.Produto>().Wait();
+        }
+
     }
 }
